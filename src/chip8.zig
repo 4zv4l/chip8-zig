56756df1bf25626b2e9d5420c36f7af5945b7e16
azz <4zv4l@protonmail.com>
2022-06-28 01:09:20 +0200

    adding more code to the decode section and more pc+=2 to execute function

--- src/chip8.zig
+++ src/chip8.zig
@@ -17,6 +17,10 @@ const Opcode_struct = struct {
     /// registers index
     X: u4 = 0,
     Y: u4 = 0,
+
+    pub fn show(self: Opcode_struct) void {
+        print("{{opcode: {d}, N: {x}, NN: {x}, NNN: {x}, X: {d}, Y: {d}}}\n", .{ self.opcode, self.N, self.NN, self.NNN, self.X, self.Y });
+    }
 };
 
 pub const Chip8 = struct {
@@ -132,11 +136,17 @@ pub const Chip8 = struct {
         print("fetched: 0x{X}\n", .{self.opcode});
 
         // decode opcode
-        const call = self.decode(self.opcode);
+        const opcode = self.decode();
+        print("0x{X:0>4} =>", .{self.opcode});
+        opcode.show();
+        self.PC += 2;
+        if (opcode.opcode == 35) {
+            std.process.exit(0);
+        }
 
         // execute if call isn't 0 (not known opcode)
-        if (call.opcode == 0) return error.UnknownOpcode;
-        self.execute(call);
+        if (opcode.opcode == 0) return error.UnknownOpcode;
+        self.execute(opcode);
 
         // update timers
         if (self.delay_timer > 0) {
@@ -156,143 +166,143 @@ pub const Chip8 = struct {
 
     /// decode opcode
     /// return 0 if not known opcode
-    fn decode(self: *Chip8, opcode: Opcode) Opcode_struct {
-        _ = opcode;
-        switch (self.opcode & 0xF000) {
-            0x0000 => switch (self.opcode & 0x000F) {
-                0x0000 => { // 00E0: clears the screen
+    fn decode(self: *Chip8) Opcode_struct {
+        // opcode to decode
+        const opcode = self.opcode;
+        switch (opcode & 0xF000) {
+            0x0000 => switch (opcode & 0x000F) {
+                0x0000 => { // 00E0
                     return Opcode_struct{ .opcode = 2 };
                 },
-                0x000E => { // 00EE: returns from subroutine
-
+                0x000E => { // 00EE
                     return Opcode_struct{ .opcode = 3 };
                 },
                 else => { // 0NNN
-                    return Opcode_struct{ .opcode = 1 };
+                    return Opcode_struct{ .opcode = 1, .NNN = @truncate(u12, opcode) };
                 },
             },
-            0x1000 => {
-                return Opcode_struct{ .opcode = 4 };
+            0x1000 => { // 1NNN
+                return Opcode_struct{ .opcode = 4, .NNN = @truncate(u12, opcode) };
             },
-            0x2000 => {
-                return Opcode_struct{ .opcode = 5 };
+            0x2000 => { // 2NNN
+                return Opcode_struct{ .opcode = 5, .NNN = @truncate(u12, opcode) };
             },
-            0x3000 => {
-                return Opcode_struct{ .opcode = 6 };
+            0x3000 => { // 3XNN
+                return Opcode_struct{ .opcode = 6, .X = @truncate(u4, opcode >> 8), .NN = @truncate(u8, opcode) };
             },
-            0x4000 => {
-                return Opcode_struct{ .opcode = 7 };
+            0x4000 => { // 4XNN
+                return Opcode_struct{ .opcode = 7, .X = @truncate(u4, opcode >> 8), .NN = @truncate(u8, opcode) };
             },
-            0x5000 => {
-                return Opcode_struct{ .opcode = 8 };
+            0x5000 => { // 5XY0
+                return Opcode_struct{ .opcode = 8, .X = @truncate(u4, opcode >> 8), .Y = @truncate(u4, opcode >> 4) };
             },
-            0x6000 => {
+            0x6000 => { // 6XNN
                 return Opcode_struct{ .opcode = 9 };
             },
-            0x7000 => {
+            0x7000 => { // 7XNN
                 return Opcode_struct{ .opcode = 10 };
             },
-            0x8000 => switch (self.opcode & 0x000F) {
-                0x0000 => {
+            0x8000 => switch (opcode & 0x000F) {
+                0x0000 => { // 8XY0
                     return Opcode_struct{ .opcode = 11 };
                 },
-                0x0001 => {
+                0x0001 => { // 8XY1
                     return Opcode_struct{ .opcode = 12 };
                 },
-                0x0002 => {
+                0x0002 => { // 8XY2
                     return Opcode_struct{ .opcode = 13 };
                 },
-                0x0003 => {
+                0x0003 => { // 8XY3
                     return Opcode_struct{ .opcode = 14 };
                 },
-                0x0004 => {
+                0x0004 => { // 8XY4
                     return Opcode_struct{ .opcode = 15 };
                 },
-                0x0005 => {
+                0x0005 => { // 8XY5
                     return Opcode_struct{ .opcode = 16 };
                 },
-                0x0006 => {
+                0x0006 => { // 8XY6
                     return Opcode_struct{ .opcode = 17 };
                 },
-                0x0007 => {
+                0x0007 => { // 8XY7
                     return Opcode_struct{ .opcode = 18 };
                 },
-                0x000E => {
+                0x000E => { // 8XYE
                     return Opcode_struct{ .opcode = 19 };
                 },
                 else => {
-                    print("opcode not known: 0x{x} .. aborting\n", .{self.opcode});
+                    print("opcode not known: 0x{x} .. aborting\n", .{opcode});
                     return Opcode_struct{ .opcode = 0 };
                 },
             },
-            0x9000 => {
+            0x9000 => { // 9XY0
                 return Opcode_struct{ .opcode = 20 };
             },
-            0xA000 => {
+            0xA000 => { // ANNN
                 return Opcode_struct{ .opcode = 21 };
             },
-            0xB000 => {
+            0xB000 => { // BNNN
                 return Opcode_struct{ .opcode = 22 };
             },
-            0xC000 => {
+            0xC000 => { // CXNN
                 return Opcode_struct{ .opcode = 23 };
             },
-            0xD000 => {
+            0xD000 => { // DXYN
                 return Opcode_struct{ .opcode = 24 };
             },
-            0xE000 => switch (self.opcode & 0x000F) {
-                0x000E => {
+            0xE000 => switch (opcode & 0x000F) {
+                0x000E => { // EX9E
                     return Opcode_struct{ .opcode = 25 };
                 },
-                0x0001 => {
+                0x0001 => { // EXA1
                     return Opcode_struct{ .opcode = 26 };
                 },
                 else => {
-                    print("opcode not known: 0x{x} .. aborting\n", .{self.opcode});
+                    print("opcode not known: 0x{x} .. aborting\n", .{opcode});
                     return Opcode_struct{ .opcode = 0 };
                 },
             },
-            0xF000 => switch (self.opcode & 0x000F) {
-                0x0003 => {
+            0xF000 => switch (opcode & 0x000F) {
+                0x0003 => { // FX33
                     return Opcode_struct{ .opcode = 33 };
                 },
-                0x0007 => {
+                0x0007 => { // FX07
                     return Opcode_struct{ .opcode = 27 };
                 },
-                0x000A => {
+                0x000A => { // FX01
                     return Opcode_struct{ .opcode = 28 };
                 },
-                0x0005 => switch (self.opcode & 0x00F0) {
-                    0x0010 => {
+                0x0005 => switch (opcode & 0x00F0) {
+                    0x0010 => { // FX15
                         return Opcode_struct{ .opcode = 29 };
                     },
-                    0x0050 => {
+                    0x0050 => { // FX55
                         return Opcode_struct{ .opcode = 34 };
                     },
-                    0x0060 => {
+                    0x0060 => { // FX65
                         return Opcode_struct{ .opcode = 35 };
                     },
                     else => {
-                        print("opcode not known: 0x{x} .. aborting\n", .{self.opcode});
+                        print("opcode not known: 0x{x} .. aborting\n", .{opcode});
                         return Opcode_struct{ .opcode = 0 };
                     },
                 },
-                0x0008 => {
+                0x0008 => { // FX18
                     return Opcode_struct{ .opcode = 30 };
                 },
-                0x000E => {
+                0x000E => { // FX1E
                     return Opcode_struct{ .opcode = 31 };
                 },
-                0x0009 => {
+                0x0009 => { // FX29
                     return Opcode_struct{ .opcode = 32 };
                 },
                 else => {
-                    print("opcode not known: 0x{x} .. aborting\n", .{self.opcode});
+                    print("opcode not known: 0x{x} .. aborting\n", .{opcode});
                     return Opcode_struct{ .opcode = 0 };
                 },
             },
             else => {
-                print("opcode not known: 0x{x} .. aborting\n", .{self.opcode});
+                print("opcode not known: 0x{x} .. aborting\n", .{opcode});
                 return Opcode_struct{ .opcode = 0 };
             },
         }
@@ -308,7 +318,9 @@ pub const Chip8 = struct {
 
             },
             2 => { // 00E0: clears the screen
-
+                self.drawFlag = true;
+                self.gfx = std.mem.zeroes([64][32]u8);
+                self.PC += 2;
             },
             3 => { // 00EE: return from subroutine
 
@@ -321,99 +333,127 @@ pub const Chip8 = struct {
             },
             6 => { // 3XNN: skips the next instruction if V[X] equals NN
 
+                self.PC += 4;
             },
             7 => { // 4XNN: skips the next instruction if V[X] is not equal NN
 
+                self.PC += 4;
             },
             8 => { // 5XY0: skips the next instruction if V[X] equals V[Y]
 
+                self.PC += 4;
             },
             9 => { // 6XNN: sets V[X] to NN
 
+                self.PC += 2;
             },
             10 => { // 7XNN: adds NN to V[X]
 
+                self.PC += 2;
             },
             11 => { // 8XY0: setx V[X] to the value of V[Y]
 
+                self.PC += 2;
             },
             12 => { // 8XY1: setx V[X] to V[X]|V[Y] (or bitwise)
                 // v[x] |= v[y]
+                self.PC += 2;
             },
             13 => { // 8XY2: sets V[X] to V[X]&V[Y] (and bitwise)
 
+                self.PC += 2;
             },
             14 => { // 8XY3: sets V[X] to V[X]^V[Y] (xor bitwise)
 
+                self.PC += 2;
             },
             15 => { // 8XY4: adds V[Y] to V[X], VF is set to 1 if a carry or 0 if not
                 // v[x] += v[y]
+                self.PC += 2;
             },
             16 => { // 8XY5: V[Y] is subtracted from V[X], VF is set to 0 when borrow and 1 if not
 
+                self.PC += 2;
             },
             17 => { // 8XY6: stores the least significant bit of V[X] in VF and then shift V[X] to the right by 1
                 // Vx >>= 1
 
+                self.PC += 2;
             },
             18 => { // 8XY7: sets V[X] to V[Y] minux V[X], VF is set to 0 when borrow and 1 if not
                 // Vx = Vy - Vx
+                self.PC += 2;
             },
             19 => { // 8XYE: store the most significant bit of V[X] in VF and then shift V[X] to the left by 1
                 // Vx <<= 1
 
+                self.PC += 2;
             },
             20 => { // 9XY0: skips the next instruction if V[X] != V[Y]
 
+                self.PC += 4;
             },
             21 => { // ANNN: sets I to the addr NNN
                 self.I = self.opcode & 0x0FFF;
                 self.PC += 2;
             },
-            22 => { // BNNN: jumps to the addr NNN + V[0]
+            22 => { // BNNN: jumps to the addr NNN + V[0], keep self.PC to the stack
 
             },
             23 => { // CXNN: sets V[X] to the result of a bitwise operation on a random number and NN
                 // Vx = rand() & NN
 
+                self.PC += 2;
             },
             24 => { // DXYN: draw sprite at coordinate (Vx, Vy) width 8px height of N px, each row of 8px is read as bit-cded starting from memory[I], VF is set to 1 if any screen pixels are flipped from set to unset and to 0 if it doesn't happen
 
+                self.PC += 2;
             },
             25 => { // EX9E: skips the next instruction if the key stored in V[X] is pressed
                 // if(key() == Vx)
 
+                self.PC += 4;
             },
             26 => { // EXA1: skips the next instruction if the key stored in V[X] is not pressed
                 // if(key() != Vx)
 
+                self.PC += 4;
             },
             27 => { // FX07: sets V[X] to the value of the delay timer
 
+                self.PC += 2;
             },
             28 => { // FX0A: A key press is awaited and then stored in V[X] (blocking)
 
+                self.PC += 2;
             },
             29 => { // FX15: sets the delay_timer to V[X]
 
+                self.PC += 2;
             },
             30 => { // FX18: sets the sound_timer to V[X]
 
+                self.PC += 2;
             },
             31 => { // FX1E: adds V[X] to I
                 // self.I += self.V[X]
+                self.PC += 2;
             },
             32 => { // FX29: sets I tot he location of the sprite for the char in V[X], char O-F (in hex) are represented by a 4x5 font
 
+                self.PC += 2;
             },
             33 => { // FX33:
 
+                self.PC += 2;
             },
             34 => { // FX55:
 
+                self.PC += 2;
             },
             35 => { // FX65:
 
+                self.PC += 2;
             },
             else => {},
         }
